590add6b31ada23aa306469c6eb6ef3349626033
zhouyilin <zhyl9697@gmail.com>
2016-12-21 22:32:38 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
+
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -13,11 +15,6 @@ class GroupsController < ApplicationController
    end
 
    def edit
-     @group = Group.find(params[:id])
-
-     if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-     end
    end
 
   def create
@@ -32,35 +29,33 @@ class GroupsController < ApplicationController
    end
 
    def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
-    if @group.update(group_params)
-      redirect_to groups_path, notice: "Update Success"
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
-    @group.destroy
-    redirect_to groups_path, alert: "Group deleted"
-  end
-
-   private
-
-   def group_params
-     params.require(:group).permit(:title, :description)
+     if @group.update(group_params)
+       redirect_to groups_path, notice: "Update Success"
+     else
+       render :edit
+     end
    end
+
+   def destroy
+     @group.destroy
+     redirect_to groups_path, alert: "Group deleted"
+   end
+
+  private
+
+ def find_group_and_check_permission
+   @group = Group.find(params[:id])
+
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
+   end
+ end
+
+  def group_params
+   params.require(:group).permit(:title, :description)
+  end
+
+ end
 
 
   def show
@@ -68,7 +63,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+    find_group_and_check_permission
   end
 
   def create
@@ -82,7 +77,9 @@ class GroupsController < ApplicationController
    end
 
    def update
-    @group = Group.find(params[:id])
+
+     find_group_and_check_permission
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -90,12 +87,12 @@ class GroupsController < ApplicationController
     end
   end
 
-   def destroy
-     @group = Group.find(params[:id])
-     @group.destroy
-     flash[:alert] = "Group deleted"
-     redirect_to groups_path
-  end
+  def destroy
+   find_group_and_check_permission
+
+   @group.destroy
+   redirect_to groups_path, alert: "Group deleted"
+ end
 
    private
 
